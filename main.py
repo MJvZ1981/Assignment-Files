c7b74e6e696a8775335de1e02499adfca289dfb8
MJvZ1981 <113166292+MJvZ1981@users.noreply.github.com>
2022-11-30 20:37:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,41 +5,28 @@ import os
 import zipfile
 import shutil
 
-# part 1
+# part 0
 path = os.getcwd()
-cache_path = os.path.join(path, "files", "cache") # de "files" dir moet erbij, omdat ik hem niet kan controleren vanuit de "files" dir
-zip_file_path = path + '\\files\data.zip'
+cache_path = os.path.join(path, "cache")
+zip_file_path = os.path.join(path, "data.zip")
 cache_dir_path = cache_path
 
+# part 1
 def clean_cache():
-    if not os.path.exists(cache_path):
-        os.mkdir(cache_path)
-    else:
-        shutil.rmtree(cache_path)
-        os.mkdir(cache_path)
-    for file in os.listdir(cache_path):
-        file_path = os.path.join(cache_path, file)
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.remove(file)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-
-clean = (clean_cache())
+    if os.path.exists(cache_path):
+        shutil.rmtree(cache_path)   
+    os.mkdir(cache_path)
 
 # part 2
 def cache_zip(zip_file_path : str, cache_dir_path : str):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(cache_dir_path)
 
-cache_zip(zip_file_path, cache_dir_path)
-
 # part 3
 def cached_files():
     os.chdir(cache_path)
     files_path = [os.path.abspath(x) for x in os.listdir(cache_path)]
     return files_path
-
-files = cached_files()
  
 # part 4
 def find_password(files : list):
@@ -49,9 +36,13 @@ def find_password(files : list):
             for line in lines:
                 if line.find('password') >= 0:
                     words = line.split(" ")
-                    for word in words:
+# vond deze persoonlijk iets generieker (omdat je niet weet waar password staat, maar de opdracht wel deze tip geeft)                   
+                    for word in words:              
                         if 'password' not in word:
                             return word 
 
-password = find_password(files)
-print(password)
+if __name__ == "__main__":
+    clean_cache()
+    cache_zip(zip_file_path, cache_dir_path)
+    cached_files()
+    print(find_password(cached_files()))
